1c1ac890af269bc8be6c4948532e7fe6fd7bc1e6
NonPlayer95 <130722174+Nonplayer95@users.noreply.github.com>
2025-06-16 22:38:11 +0900

    unreal c++

---
+++ Source/MyUnreal/MyUnreal.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyUnreal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyUnreal, "MyUnreal" );
---
+++ Source/MyUnreal/Private/MyActor.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+
+	start = FVector2D(0.0f, 0.0f);
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UE_LOG(LogTemp, Warning, TEXT("== MyActor가 스폰되었습니다! 랜덤 이동을 시작합니다 =="));
+	move();
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+int32 AMyActor::step()
+{
+	return FMath::RandRange(0, 1);
+}
+
+void AMyActor::move()
+{
+
+}
